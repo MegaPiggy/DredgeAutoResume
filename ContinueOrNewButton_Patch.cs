d50593c874689537b5d3098524c6a156bad5c2dc
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-07-01 18:57:57 -0400

    patch button instead

---
+++ ContinueOrNewButton_Patch.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Winch.Core;
+
+namespace AutoResume.Patches
+{
+	[HarmonyPatch(typeof(ContinueOrNewButton), "Start")]
+	public static class ContinueOrNewButton_Patch
+	{
+		public static bool didOnce = false;
+
+		public static void Postfix(ContinueOrNewButton __instance)
+		{
+			var button = __instance.GetComponent<BasicButton>();
+			var wrapper = __instance.GetComponent<BasicButtonWrapper>();
+			if (!didOnce)
+			{
+				didOnce = true;
+				ClickContinueButton(button, wrapper);
+			}
+		}
+
+		private static void ClickContinueButton(BasicButton button, BasicButtonWrapper wrapper)
+		{
+			WinchCore.Log.Info("Auto resuming");
+			wrapper.PlaySubmitSFX();
+			button.onClick.Invoke();
+			wrapper.OnClick.Invoke();
+		}
+	}
+}
--- TitleController_Patch.cs
+++ TitleController_Patch.cs
@@ -10,25 +10,9 @@ namespace AutoResume.Patches
 	[HarmonyPatch(typeof(TitleController), "Awake")]
 	public static class TitleController_Patch
 	{
-		public static bool didOnce = false;
-
 		public static void Prefix(TitleController __instance)
 		{
 			var buttons = GameObject.Find("Canvases/MenuCanvas/ButtonContainer");
-			var continueNew = buttons.GetComponentInChildren<ContinueOrNewButton>(true);
-			var button = continueNew.GetComponent<BasicButton>();
-			var wrapper = continueNew.GetComponent<BasicButtonWrapper>();
-			if (!didOnce)
-			{
-				didOnce = true;
-				continueNew.StartCoroutine(continueNew.WaitFrames(1, () =>
-				{
-					WinchCore.Log.Info("Auto resuming");
-					wrapper.PlaySubmitSFX();
-					button.onClick.Invoke();
-					wrapper.OnClick.Invoke();
-				}));
-			}
 			var discord = buttons.GetComponentInChildren<DiscordButton>(true);
 			GameObject.DestroyImmediate(discord.GetComponent<SKUSpecificDisabler>());
 			discord.gameObject.SetActive(true);
